eb840d62d1e01bab1cee26df4da0867bfab2f99a
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2025-01-08 15:44:04 -0300

    Skeleton
    App de ejercicio practico

---
+++ SkeletonApp-D/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cl.duoc.cmartinezs.skeletonapp',
+  appName: 'SkeletonApp',
+  webDir: 'www'
+};
+
+export default config;
