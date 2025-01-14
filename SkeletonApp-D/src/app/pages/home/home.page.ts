cc24e782aa363a98d595f9c166ba71c2e91e4910
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2025-01-14 15:45:48 -0300

    localstorage

--- SkeletonApp-D/src/app/pages/home/home.page.ts
+++ SkeletonApp-D/src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ViewWillEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,13 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage implements OnInit {
+export class HomePage implements ViewWillEnter {
 
   loggedUser: string = ''
 
+  firstname!: string;
+  lastname!: string;
+  edLvl!: string;
+  birthday!: string;
+
   constructor() {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
     this.loggedUser = localStorage.getItem('loggedUser') ?? 'guest'
+    const dataJson = localStorage.getItem(`data_${this.loggedUser}`) ?? '{}'
+    const data = JSON.parse(dataJson);
+    this.firstname = data.firstname;
+    this.lastname = data.lastname;
+    this.edLvl = data.edLvl;
+    this.birthday = data.birthday;
+  }
+
+  save(){
+    const data = {
+      firstname: this.firstname,
+      lastname: this.lastname,
+      edLvl: this.edLvl,
+      birthday: this.birthday
+    }
+
+    localStorage.setItem(`data_${this.loggedUser}`, JSON.stringify(data))
   }
 }
